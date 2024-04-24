1b63a9b914b68d4801db90ebc97497b11ad04fe9
Hanzzz123 <aanzs496@gmail.com>
2024-04-24 23:48:36 +0700

    create user model (mvc)

---
+++ server/src/models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+//schema
+const userSchema = mongoose.Schema({
+    firstname:{
+        required: [true, 'First name is required'],
+        type: String,
+    },
+    lastname: {
+        required: [true, 'Last name is required'],
+        type: String,
+    },
+    email: {
+        required: [true, 'email is required'],
+        type: String,
+    },
+    password: {
+        required: [true, 'password is required'],
+        type: String,
+    },
+    isAdmin:{
+        type: Boolean,
+    }
+},
+    {
+        timestamp: true
+    })
+
+//compile schema into model
+const User = mongoose.model('User', userSchema);
+module.exports = User;
