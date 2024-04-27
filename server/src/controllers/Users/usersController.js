92963398ce7444a00de2dbf4d0c629b85b43b5ab
Hanzzz123 <aanzs496@gmail.com>
2024-04-27 19:45:29 +0700

    complete register controller

--- server/src/controllers/Users/usersController.js
+++ server/src/controllers/Users/usersController.js
@@ -1,8 +1,25 @@
+const User = require("../../models/User");
+
+
+
 
 //Register
-const registerUser = (req,res)=>{
-    res.json({user:"admin"})
+const registerUser = async (req,res)=>{
+    const {email, firstname, lastname, password} = req?.body;
+    try{
+        //check if user exists
+        const userExists = await User.findOne({email});
+        if(userExists){
+            res.json('User exists');
+        }
+        const user = await User.create({email, firstname, lastname, password});
+        res.status(200).json(user);
+    }catch (error){
+        res.json(error);
+    }
 };
+
+
 
 module.exports = {
     registerUser
