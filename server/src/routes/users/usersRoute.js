7964a2a33d2bb41c8bf42c755e02e1707c40aae5
Hanzzz123 <aanzs496@gmail.com>
2024-05-02 21:15:38 +0700

    export userRoute

--- server/src/routes/users/usersRoute.js
+++ server/src/routes/users/usersRoute.js
@@ -1,7 +1,7 @@
 import express from "express";
 import {registerUser} from "../../controllers/Users/usersController.js";
 
-export const userRoute = express.Router();
+const userRoute = express.Router();
 
 userRoute.post('/register',registerUser)
 
