9fae45e8e01cad318c8ba453240cff130e745d46
Hanzzz123 <aanzs496@gmail.com>
2024-05-03 23:56:21 +0700

    add login endpoint route

--- server/src/routes/users/usersRoute.js
+++ server/src/routes/users/usersRoute.js
@@ -1,9 +1,13 @@
 import express from "express";
-import {registerUser,fetchUsers} from "../../controllers/Users/usersController.js";
+import {registerUser,
+    fetchUsers,
+    loginUserController
+} from "../../controllers/Users/usersController.js";
 
 const userRoute = express.Router();
 
 userRoute.post('/register',registerUser);
+userRoute.post('/login',loginUserController);
 userRoute.get("/",fetchUsers);
 
 export default userRoute;
