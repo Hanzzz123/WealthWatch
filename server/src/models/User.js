e48167cbbdaa5e1a8a6f914cbd734f7ae04ec0fc
Hanzzz123 <aanzs496@gmail.com>
2024-05-03 23:54:54 +0700

    add verify password

--- server/src/models/User.js
+++ server/src/models/User.js
@@ -37,6 +37,10 @@ userSchema.pre('save', async function (next) {
     next();
 })
 
+//verify password
+userSchema.methods.isPasswordMatch = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword,this.password)
+}
 //compile schema into model
 export const User = mongoose.model('User', userSchema);
 export default {User,userSchema}
