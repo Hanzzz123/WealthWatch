9185afbfbaf9bdf256c3113df2fff27605e7efe9
Hanzzz123 <aanzs496@gmail.com>
2024-05-03 23:45:34 +0700

    use .env variable url

--- server/src/config/dbConnect.js
+++ server/src/config/dbConnect.js
@@ -1,13 +1,16 @@
 import mongoose from "mongoose";
 
 export const dbConnect = async () =>{
+    // console.log(process.env.MONGO_URL)
     try{
-        await mongoose.connect("mongodb://localhost/WealthWatch", {
+        await mongoose.connect(process.env.MONGO_URL, {
+
         });
         console.log(`DB Connected succesfully`)
+
     }catch (error){
         console.log(`Error ${error.message}`)
     }
 };
 
-export default dbConnect();
+export default dbConnect;
