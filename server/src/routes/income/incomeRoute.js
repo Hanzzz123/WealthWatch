5b91d404041aebb6313641597d69c1a96d2dbbc6
Hanzzz123 <aanzs496@gmail.com>
2024-05-06 17:45:26 +0700

    add update income and delete income route endpoint

--- server/src/routes/income/incomeRoute.js
+++ server/src/routes/income/incomeRoute.js
@@ -2,7 +2,9 @@ import express from "express";
 import {
     createIncomeController,
     fetchAllIncomeController,
-    fetchIncomeDetailController
+    fetchIncomeDetailController,
+    updateIncomeController,
+    deleteIncomeController
 } from "../../controllers/Income/incomeController.js";
 
 export const incomeRoute = express.Router();
@@ -10,6 +12,7 @@ export const incomeRoute = express.Router();
 incomeRoute.post('/',createIncomeController )
 incomeRoute.get('/',fetchAllIncomeController)
 incomeRoute.get('/:id',fetchIncomeDetailController)
-
+incomeRoute.put('/:id',updateIncomeController)
+incomeRoute.delete("/:id",deleteIncomeController)
 
 export default {incomeRoute}
