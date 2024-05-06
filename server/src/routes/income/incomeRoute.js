cf26e270ca1404b2a8cc81f4a6b515761f63b9cd
Hanzzz123 <aanzs496@gmail.com>
2024-05-06 16:53:24 +0700

    add fetch income detail controller endpoint route

--- server/src/routes/income/incomeRoute.js
+++ server/src/routes/income/incomeRoute.js
@@ -1,10 +1,15 @@
 import express from "express";
-import {createIncomeController, fetchAllIncomeController} from "../../controllers/Income/incomeController.js";
+import {
+    createIncomeController,
+    fetchAllIncomeController,
+    fetchIncomeDetailController
+} from "../../controllers/Income/incomeController.js";
 
 export const incomeRoute = express.Router();
 
 incomeRoute.post('/',createIncomeController )
 incomeRoute.get('/',fetchAllIncomeController)
+incomeRoute.get('/:id',fetchIncomeDetailController)
 
 
 export default {incomeRoute}
