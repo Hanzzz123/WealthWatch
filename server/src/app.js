dd9d92501d5254770ff19d4a205ff046a2173d0f
Hanzzz123 <aanzs496@gmail.com>
2024-05-06 20:55:00 +0700

    add expenseRoute

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,7 @@ import userRoute from "./routes/users/usersRoute.js";
 import {errorHandler,notFound} from "./middlewares/errorMiddleware.js";
 import dotenv from "dotenv"
 import {incomeRoute} from "./routes/income/incomeRoute.js";
+import {expenseRoute} from "./routes/expenses/expenseRoute.js";
 
 
 export const app = express();
@@ -20,6 +21,7 @@ app.use(express.json())
 //routes
 app.use('/api/users', userRoute)
 app.use('/api/income',incomeRoute)
+app.use('/api/expense',expenseRoute)
 
 //error
 app.use(notFound);
