7fb71541fc010d7fd29ae974c3d68218b482688c
Hanzzz123 <aanzs496@gmail.com>
2024-05-09 22:58:28 +0700

    add income pagination

--- server/src/controllers/Income/incomeController.js
+++ server/src/controllers/Income/incomeController.js
@@ -21,7 +21,7 @@ export const createIncomeController = expressAsyncHandler(async (req, res) => {
 export const fetchAllIncomeController = expressAsyncHandler(async (req, res) => {
     const {page} = req.query;
     try {
-        const income = await Income.paginate({},{limit:10, page:Number(page)});
+        const income = await Income.paginate({},{limit:10, page:Number(page),populate:"user"});
         res.json(income);
     } catch (error){
         res.json(error);
